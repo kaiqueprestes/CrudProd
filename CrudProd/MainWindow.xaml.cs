f9680582c99e9b4c87653be7e1f37002d9e63222
kaique.prestes <kaique.prestes@exview.com.br>
2022-06-02 14:20:22 -0300

    Kaique --> Ajustes conexão com bando de dados

--- CrudProd/MainWindow.xaml.cs
+++ CrudProd/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace CrudProd
 {
@@ -20,9 +21,60 @@ namespace CrudProd
     /// </summary>
     public partial class MainWindow : Window
     {
+        MySqlConnection conexaoDb;
+        MySqlCommand executrQuery;
+        MySqlDataAdapter dataAdp;
+        MySqlDataReader dataRd;
+        string querySql;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dataHoraCadastro = DateTime.Now.ToString("yyyy/dd/MM HH:mm:ss"); //DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")
+
+                var ativo = "0";
+
+                if (checkAtivo.IsChecked == true)
+                {
+                    ativo = "1";
+                }
+
+                conexaoDb = new MySqlConnection("Server = localhost; Database = testdev; Uid = root; Pwd = root;");
+
+                querySql = "INSERT INTO PRODUTO (descricao, codGrupo, codBarra, precoCusto, precoVenda,dataHoraCadastro, ativo)" +
+                    "VALUES(@descricao,@codGrupo, @codBarra,@precoCusto,@precoVenda,@dataHoraCadastro,@ativo)";
+
+                executrQuery = new MySqlCommand(querySql, conexaoDb);
+                executrQuery.Parameters.AddWithValue("@descricao", txtDescricao.Text);
+                executrQuery.Parameters.AddWithValue("@codBarra", txtCodBarra.Text);
+                executrQuery.Parameters.AddWithValue("@codGrupo", "1");
+                executrQuery.Parameters.AddWithValue("@precoCusto", txtPrecoCusto.Text);
+                executrQuery.Parameters.AddWithValue("@precoVenda", txtPrecoVenda.Text);
+                executrQuery.Parameters.AddWithValue("@dataHoraCadastro", dataHoraCadastro);
+                executrQuery.Parameters.AddWithValue("@ativo", ativo);
+
+                conexaoDb.Open();
+
+                executrQuery.ExecuteNonQuery();
+                MessageBox.Show("Produto Cadastrado!");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexaoDb.Close();
+                conexaoDb = null;
+                executrQuery = null;
+            }
         }
     }
 }
